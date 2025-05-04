843b1b3b1064dcb15c44ea6448b84adce8e02989
Subu56-creator <samanjasem835@gmail.com>
2025-05-04 23:00:12 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleFont() {
-    let title = document.querySelector('.gothic-font');
-    if (title.style.fontFamily.includes('UnifrakturMaguntia')) {
-        title.style.fontFamily = 'Arial, sans-serif';
-    } else {
-        title.style.fontFamily = "'UnifrakturMaguntia', cursive";
-    }
-}
